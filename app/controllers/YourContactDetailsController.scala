94911e306b7c5e39b1f7e7ce86b96e582a4ee84d
Adam Cooper <61467301+AdamCoops@users.noreply.github.com>
2023-02-03 15:27:55 +0000

    MMCA-3745-Update link to contact details

--- app/controllers/YourContactDetailsController.scala
+++ app/controllers/YourContactDetailsController.scala
@@ -21,13 +21,13 @@ import config.AppConfig
 import connectors.CustomsFinancialsSessionCacheConnector
 import domain.CompanyAddress
 import javax.inject.Inject
+import play.api.{Logger, LoggerLike}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import services.DataStoreService
+import services.{ApiService, DataStoreService}
 import uk.gov.hmrc.http.SessionId
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.your_contact_details.your_contact_details
-
 import scala.concurrent.{ExecutionContext, Future}
 
 class YourContactDetailsController @Inject()(authenticate: IdentifierAction,
@@ -39,9 +39,10 @@ class YourContactDetailsController @Inject()(authenticate: IdentifierAction,
                                        (implicit val appConfig: AppConfig, ec: ExecutionContext)
                                        extends FrontendController(mcc) with I18nSupport {
 
+  val log: LoggerLike = Logger(this.getClass)
+
   def onPageLoad(): Action[AnyContent] = authenticate async { implicit request =>
     for {
-
       email <- dataStoreService.getEmail(request.user.eori).flatMap {
         case Left(_) => Future.successful(InternalServerError)
         case Right(email) => Future.successful(email.value)
@@ -60,8 +61,9 @@ class YourContactDetailsController @Inject()(authenticate: IdentifierAction,
         countryCode = companyAddress.countryCode
       )
 
-      sessionId = hc.sessionId.getOrElse(SessionId("Missing Session ID"))
+      sessionId = hc.sessionId.getOrElse({log.error("Missing SessionID"); SessionId("Missing Session ID")})
       accountNumber <- sessionCacheConnector.getAccountNumbers(request.user.eori, sessionId.value)
+
 
     } yield {
       Ok(view(request.user.eori, accountNumber.getOrElse(Seq("")),
