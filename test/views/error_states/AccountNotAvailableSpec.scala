7b1f56f8c4540318c3120febaa5e58cbcbeaa331
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2023-12-27 13:24:42 +0000

    [MMCA-4406] adding AccountNotAvailableSpec

---
+++ test/views/error_states/AccountNotAvailableSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.html.error_states
+
+import utils.SpecBase
+import config.AppConfig
+import org.jsoup.nodes.Document
+import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import org.jsoup.Jsoup
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import views.html.error_states.account_not_available
+
+
+class AccountNotAvailableSpec extends SpecBase {
+
+  "AccountNotAvailable view" should {
+    "render correctly" in new Setup {
+        view.text().contains(messages(app)("cf.customs-financials-home.partial.heading")) mustBe true
+        view.text().contains(messages(app)("cf.customs-financials-home.partial.text")) mustBe true
+        view.text().contains(messages(app)("cf.customs-financials-home.import-vat.title")) mustBe true
+        view.text().contains(messages(app)("cf.customs-financials-home.pvat.title")) mustBe true
+        view.text().contains(messages(app)("cf.customs-financials-home.securities.title")) mustBe true
+        view.text().contains(messages(app)("cf.import-vat.view-certificates.description")) mustBe true
+        view.text().contains(messages(app)("cf.postponed-vat.view-certificates.description")) mustBe true
+        view.text().contains(messages(app)("cf.securities.view-statements.description")) mustBe true
+    }
+  }
+
+  trait Setup {
+    val app = application().build()
+    val eori = "GB000000000001"
+    val notificationMessageKeys = Seq("key1", "key2")
+
+    implicit val appConfig = app.injector.instanceOf[config.AppConfig]
+    implicit val msg: Messages = messages(app)
+    implicit val request = FakeRequest("GET", "/some/resource/path")
+
+    val view: Document = Jsoup.parse(
+      app.injector.instanceOf[account_not_available].apply(eori, notificationMessageKeys).body
+    )
+  }
+}
