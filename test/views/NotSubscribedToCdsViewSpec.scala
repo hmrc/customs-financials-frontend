cf92c77febbe55a6b2bf8ebe63c85aa9bdd1d631
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2024-04-14 23:10:38 +0100

    [MMCA-4540] Fix tests

--- test/views/NotSubscribedToCdsViewSpec.scala
+++ test/views/NotSubscribedToCdsViewSpec.scala
@@ -18,8 +18,11 @@ package views
 
 import config.AppConfig
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import play.api.Application
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
 import play.api.test.Helpers.running
 import utils.SpecBase
@@ -30,29 +33,34 @@ class NotSubscribedToCdsViewSpec extends SpecBase {
   "Not subscribed to cds view" should {
     "display header as non-link text" in new Setup {
       running(app) {
-        view.getElementsByClass("hmrc-header__service-name--linked").text mustBe "View your customs financial accounts"
+        view.getElementsByClass("govuk-header__link")
+          .text mustBe "GOV.UK View your customs financial accounts"
       }
     }
 
     "display page heading" in new Setup {
       running(app) {
-        view.getElementsByTag("h1").text mustBe "To continue you need to subscribe to the Customs Declaration Service (CDS)"
+        view.getElementsByTag("h1")
+          .text mustBe "To continue you need to subscribe to the Customs Declaration Service (CDS)"
       }
     }
 
     "display get access to cds service links" in new Setup {
       running(app) {
-        view.containsLinkWithText("/customs/register-for-cds", "Economic Operator and Registration Identification (EORI) number (opens in a new window or tab)")
+        view.containsLinkWithText(
+          "/customs/register-for-cds",
+          "Economic Operator and Registration Identification (EORI) number (opens in a new window or tab)")
+
         view.containsLinkWithText(appConfig.subscribeCdsUrl, "get access to CDS (opens in a new window or tab)")
       }
     }
   }
 
   trait Setup extends I18nSupport {
-    implicit val request = FakeRequest("GET", "/some/resource/path")
-    val app = application().build()
-    implicit val appConfig = app.injector.instanceOf[AppConfig]
-    val view = Jsoup.parse(app.injector.instanceOf[not_subscribed_to_cds].apply().body)
+    implicit val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("GET", "/some/resource/path")
+    val app: Application = application().build()
+    implicit val appConfig: AppConfig = app.injector.instanceOf[AppConfig]
+    val view: Document = Jsoup.parse(app.injector.instanceOf[not_subscribed_to_cds].apply().body)
 
     override def messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
   }
