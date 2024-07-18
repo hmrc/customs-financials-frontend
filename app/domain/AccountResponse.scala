7e7e7acb728857643404a6abb30a0288b805162c
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-07-18 11:35:58 +0100

    [MMCA-4831] Update the default IOM flag value to None

--- app/domain/AccountResponse.scala
+++ app/domain/AccountResponse.scala
@@ -24,7 +24,7 @@ case class AccountResponse(number: String,
                            accountStatus: Option[CDSAccountStatus],
                            accountStatusID: Option[CDSAccountStatusId],
                            viewBalanceIsGranted: Boolean,
-                           isleOfManFlag: Option[Boolean] = Some(false))
+                           isleOfManFlag: Option[Boolean] = None)
 
 object AccountResponse {
   implicit val reads: Reads[AccountResponse] = Json.reads[AccountResponse]
