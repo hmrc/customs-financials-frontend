adf7606fad61c98c99b6d9bd751d54a63b869234
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2025-02-04 14:08:07 +0000

    MMCA-5154 Update other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,21 +18,21 @@ import sbt.*
  */
 
 object AppDependencies {
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-partials-play-30" % "10.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.4.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.11.0",
     ws,
-    "org.typelevel" %% "cats-core" % "2.12.0",
+    "org.typelevel" %% "cats-core" % "2.13.0",
     "uk.gov.hmrc" %% "tax-year" % "5.0.0",
     "org.webjars.npm" % "moment" % "2.30.1"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.jsoup" % "jsoup" % "1.18.1" % Test,
+    "org.jsoup" % "jsoup" % "1.18.3" % Test,
     "org.scalatestplus" %% "mockito-4-11" %"3.2.18.0" % Test,
   )
 }
