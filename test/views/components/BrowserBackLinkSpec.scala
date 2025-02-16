8d74df900f90027e9af91ffe62ab903ef6087b67
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2025-02-16 23:51:56 +0000

    [MMCA-5174] Added BrowserBackLink component with dynamic href and JS functionality

---
+++ test/views/components/BrowserBackLinkSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.Application
+import play.api.test.Helpers.{contentAsString, running}
+import play.twirl.api.HtmlFormat
+import utils.SpecBase
+import utils.MustMatchers
+import play.api.test.Helpers.defaultAwaitTimeout
+import views.html.components.browser_back_link
+
+class BrowserBackLinkSpec extends SpecBase with MustMatchers {
+
+  "BrowserBackLink component" should {
+
+    "render the back link with correct attributes and text" in new Setup {
+      running(app) {
+        val output: HtmlFormat.Appendable = browserBackLinkView(
+          href = "/url",
+          content = "Back"
+        )(messages(app))
+
+        val html: Document = Jsoup.parse(contentAsString(output))
+
+        val linkElement = html.getElementById("browser-back-link")
+        linkElement.attr("href") mustBe "/url"
+        linkElement.attr("class") must include("govuk-back-link govuk-!-margin-bottom-9")
+        linkElement.text() mustBe "Back"
+      }
+    }
+  }
+
+  trait Setup {
+    val app: Application = application().build()
+    val browserBackLinkView: browser_back_link = app.injector.instanceOf[browser_back_link]
+
+  }
+}
