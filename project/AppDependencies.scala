605303030beb5784346287b0488fe2cebb14cca4
Neetha-sudo <83287552+Neetha-sudo@users.noreply.github.com>
2026-08-28 10:54:18 +0100

    DCWL-4865-add test-repositories to repository.yaml

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,12 +23,12 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.11.0",
     ws,
     "org.typelevel"     %% "cats-core"                  % "2.13.0",
     "uk.gov.hmrc"       %% "tax-year"                   % "6.0.0",
     "org.webjars.npm"    % "moment"                     % "2.30.1",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % "2.12.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
